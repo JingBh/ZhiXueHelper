df387bfb798de79ff70bab088bf39493a823f1ff
JingBh <jingbohao@yeah.net>
2022-01-22 11:32:58 +0800

    feat: add about page

--- app/src/main/java/top/jingbh/zhixuehelper/ui/page/AboutActivity.kt
+++ app/src/main/java/top/jingbh/zhixuehelper/ui/page/AboutActivity.kt
@@ -1,5 +1,7 @@
 package top.jingbh.zhixuehelper.ui.page
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import top.jingbh.zhixuehelper.databinding.ActivityAboutBinding
@@ -14,5 +16,11 @@ class AboutActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.topAppBar.setNavigationOnClickListener { finish() }
+
+        binding.viewSource.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/JingBh/ZhiXueHelper")
+            startActivity(intent)
+        }
     }
 }
